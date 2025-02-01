a2ca851c9b96ba091854708ff0f3403513d764a4
LENOVO <LENOVO@DESKTOP-H8T31KO>
2025-02-01 13:57:27 +0530

    executed as TestNG XML

--- CucumberMaven/src/test/java/Feature/ValidateLogin.feature
+++ CucumberMaven/src/test/java/Feature/ValidateLogin.feature
@@ -1,5 +1,7 @@
 Feature: Login Functionality
 
+
+@login
   Scenario: Validate the Login fuctionality of DemoWebshop
     Given User launch DemoWebshop page
     When User entered credentials
--- CucumberMaven/src/test/java/StepDefinition/ValidateLogin.java
+++ CucumberMaven/src/test/java/StepDefinition/ValidateLogin.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-
+import org.testng.Assert;
 import PageObjects.LoginPageObjects;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -15,83 +15,74 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import junit.framework.Assert;
 import utilities.BaseTest;
 
 public class ValidateLogin extends BaseTest{
 
 	WebDriver driver;
-	
+
 	String email = "rao.simha@abc.com";
-	
+
 	//LoginPageObjects loginPageObj = PageFactory.initElements(driver, LoginPageObjects.class);
-	
-	@Before
-	public void launchBrowser()
-	{
-		driver = BaseTest.setBrowser();
-	}
+
 	
 
 	@Given("User launch DemoWebshop page")
-	public void user_launch_demowebshop_page() {
-	  
-	
+	public void user_launch_demowebshop_page() {	  
+
+		driver = BaseTest.setBrowser();
 		driver.get("https://demowebshop.tricentis.com/login/");
-		
-		
+
 	}
 
 	@When("User entered credentials")
 	public void user_entered_credentials() {
-		
+
 		LoginPageObjects loginPageObj = PageFactory.initElements(driver, LoginPageObjects.class);
 		loginPageObj.emailID.sendKeys(email);
-		loginPageObj.userPassword.sendKeys("abc@1234");
-	 
-		
+		loginPageObj.userPassword.sendKeys("abc@12345");
+
+
 		/*
 		WebElement Username = driver.findElement(By.id("Email"));
 		WebElement Password = driver.findElement(By.id("Password"));
-		
+
 		Username.sendKeys(email);
 		Password.sendKeys("abc@12345");
-		*/
-		
+		 */
+
 	}
 
 	@And("User click Login button")
 	public void user_click_login_button() {
 		LoginPageObjects loginPageObj = PageFactory.initElements(driver, LoginPageObjects.class);
 		loginPageObj.loginButton.click();
-		
+
 		/*
 		WebElement loginButton = driver.findElement(By.xpath("//*[@class='button-1 login-button']"));		
 		loginButton.click();
-		*/
-		
+		 */
+
 	}
 
 	@Then("User verified Login page")
 	public void user_verified_login_page() {
-		
+
 		String profilename = "rao.simha@abc.com";
-		
+
 		WebElement loginProfileName = driver.findElement(By.xpath("//*[contains(text(),'"+profilename+"')]"));		
 		Assert.assertEquals(email, loginProfileName.getText());
 		System.out.println("Login is successful");
-		
+
 	}
 
-	
-	
-	 @After
-	    public void captureScreenshot(Scenario scenario) {
-	        if (scenario.isFailed()) {
-	            System.out.println("Scenario failed. Taking screenshot...");
-	            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-	            scenario.attach(screenshot, "image/png", scenario.getName() );
-	        }
-	    }
-	
+	@After
+	public void captureScreenshot(Scenario scenario) {
+		if (scenario.isFailed()) {
+			System.out.println("Scenario failed. Taking screenshot...");
+			byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenshot, "image/png", scenario.getName() );
+		}
+	}
+
 }
